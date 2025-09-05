bb33099cb3db6424afd8bbbd6a6ef422f7805242
Rukhsat <rhjim2005@gmail.com>
2025-09-06 01:07:49 +0600

    added word functionality

--- script.js
+++ script.js
@@ -24,14 +24,23 @@ let loadLevelWord = (id) =>{
 let displayLevelWords = (words)=> {
     let wordContainer = document.getElementById('wordContainer');
     wordContainer.innerHTML =''
+    if(words.length === 0){
+        wordContainer.innerHTML =`
+        <div class="text-center col-span-full my-7">
+                <img src="assets/alert-error.png" class="mx-auto mb-3">
+                <p><span class="bng">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</span></p>
+                <h2 class="font-semibold text-[34px] mt-3"><span class="bng">নেক্সট Lesson এ যান</span></h2>
+            </div>
+        `
+        return
+    };
     for (const word of words) {
-        console.log(word);
         let card = document.createElement('div')
         card.innerHTML = `
             <div class="wordCard bg-white p-6 text-center rounded-lg">
-                <h3 class="font-bold text-xl mb-3">${word.word}</h3>
+                <h3 class="font-bold text-xl mb-3">${word.word ? word.word : 'শব্দ পাওয়া যায়নি'}</h3>
                 <p class="mb-2">Meaning /Pronounciation</p>
-                <h4><span class="bng text-xl font-bold text-gray-700">"${word.meaning} / ${word.pronunciation}"</span></h4>
+                <h4><span class="bng text-xl font-bold text-gray-700">"${word.meaning ? word.meaning: 'অর্থ পাওয়া যায়নি' } / ${word.pronunciation ?word.pronunciation : 'উচ্চারণ পাওয়া যায়নি' }"</span></h4>
                 <div class="flex text-white justify-between mt-5">
                         <button class="btn"><i class="fa-solid fa-circle-info"></i></button>
                         <button class="btn"><i class="fa-solid fa-volume-high"></i></button>
