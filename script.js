4c2d862474ef784393cdd67f11f9beaa3b99f6f8
Rukhsat <rhjim2005@gmail.com>
2025-09-05 22:34:43 +0600

    Load words by Level

--- script.js
+++ script.js
@@ -9,9 +9,16 @@ let displayLesson = (lessons) =>{
     for (const lesson of lessons) {
         let levelBox = document.createElement('div')
         levelBox.innerHTML = `
-        <button class="btn btn-outline btn-primary" href=""><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+        <button onclick= "loadLevelWord('${lesson.level_no}')" class="btn btn-outline btn-primary" href=""><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
         `
         levelContainer.appendChild(levelBox)
     }
+}
+// load level words
+let loadLevelWord = (id) =>{
+    let url = `https://openapi.programming-hero.com/api/word/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWords(data))
 }
 loadLesson()
